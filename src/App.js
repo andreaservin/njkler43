aca44dfb5c2eafd86da8c7a56251220e05bc69b7
Andrea <andiserm@gmail.com>
2023-01-27 20:29:37 -0300

    Agrego solucion a tests fallidos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import posts from './posts'
-
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      word: '',
+      list: posts,
+    }
+  }
+
+  handleChangeWord = (e) => {
+    this.setState({ word: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+        <div className='filter'>
+          <input type='text' placeholder='Ingresa el término de búsqueda' value={this.state.word} onChange={this.handleChangeWord.bind(this)} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.state.list.map(({ url, title, image }, key) => {
+            if (title.toLocaleLowerCase().includes(this.state.word.toLocaleLowerCase())) {
+              return (
+                <li key={key}>
+                  <a href={url}>
+                    <img src={image} alt={title} />
+                  </a>
+                  <p>{title}</p>
+                </li>
+              )
+            }
+          })}
         </ul>
       </div>
     )
   }
 }
 
-
 export default App
-
-
